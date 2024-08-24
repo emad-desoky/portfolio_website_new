13679c423e2b5620f6d8002f2b7a6bfc77534ffc
emad-desoky <deskoemad@gmail.com>
2024-08-24 15:55:59 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import * as Sentry from "@sentry/nextjs";
-
-// Initialize Sentry
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {" "}
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
