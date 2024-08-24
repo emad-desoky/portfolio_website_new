30f22f11e0e9fa39dbe36c8a1b4744c20c1b26c7
emad-desoky <deskoemad@gmail.com>
2024-08-24 16:39:20 +0300

    sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,15 +5,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-    dsn: "https://d4b47349c5bb7106fecf5af589cf9759@o4507828895940608.ingest.de.sentry.io/4507828903477328",
+  dsn: "https://d4b47349c5bb7106fecf5af589cf9759@o4507828895940608.ingest.de.sentry.io/4507828903477328",
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 
-    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: process.env.NODE_ENV === 'development',
-
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
 });
